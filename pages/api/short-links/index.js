a61f2c00a59eb897a2e9fcb989e65ae8426e9f38
choseongchan3712 <choseongchan3712@naver.com>
2025-07-14 23:19:15 +0900

    res

--- pages/api/short-links/index.js
+++ pages/api/short-links/index.js
@@ -2,7 +2,37 @@
 // res: Response 객체: 서버가 클라이언트에게 응답할 때 사용하는 객체입니다. (예: 텍스트, JSON, 에러, 상태코드 등을 보낼 수 있습니다.
 
 const handler = (req, res) => {
-    res.send('Hello API!');
+  switch (req.method) {
+    case "POST":
+      res.status(201).send({
+        title: "위키피디나 Next.js",
+        url: "https://en.wikipedia.org/wiki/Next.js",
+      });
+      break;
+
+    case "GET":
+      res.send([
+        {
+          id: "abc",
+          title: "위키피디나 Next.js",
+          url: "https://en.wikipedia.org/wiki/Next.js",
+        },
+        {
+          id: "def",
+          title: "코드잇 자유게시판",
+          url: "https://codeit.kr/community.general",
+        },
+        {
+          id: "ghi",
+          title: "코드잇 질문답변",
+          url: "https://www.codeit.kr/community/questions",
+        },
+      ]);
+      break;
+
+      default:
+        res.status(404).send();
+  }
 };
 
 export default handler;
