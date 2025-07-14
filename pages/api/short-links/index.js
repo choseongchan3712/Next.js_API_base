283602430da70d0bd46a5ab3565b49f9760fb213
choseongchan3712 <choseongchan3712@naver.com>
2025-07-15 00:05:08 +0900

    api_connet

--- pages/api/short-links/index.js
+++ pages/api/short-links/index.js
@@ -1,7 +1,13 @@
 // req: Request 객체: 클라이언트가 서버에 요청한 내용이 담겨 있습니다. (예: 요청 URL, 파라미터, 본문(body), 헤더 등
 // res: Response 객체: 서버가 클라이언트에게 응답할 때 사용하는 객체입니다. (예: 텍스트, JSON, 에러, 상태코드 등을 보낼 수 있습니다.
 
-const handler = (req, res) => {
+import dbConnect from "@/db/dbConnect";
+import mongoose from "mongoose";
+
+const handler = async (req, res) => {
+    await dbConnect();
+    console.log(mongoose.connection.readyState);
+
   switch (req.method) {
     case "POST":
       res.status(201).send({
